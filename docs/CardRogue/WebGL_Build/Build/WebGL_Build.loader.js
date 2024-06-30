7dcc1854d3bdc776eab772fc69eb2a251d41a5b1
tan90-Titan <cirno-sp-shoot@docomo.ne.jp>
2024-07-01 00:12:03 +0900

    シーンの共通処理を修正

--- docs/CardRogue/WebGL_Build/Build/WebGL_Build.loader.js
+++ docs/CardRogue/WebGL_Build/Build/WebGL_Build.loader.js
@@ -55,7 +55,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 65744083,
+    wasmFileSize: 65744110,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
